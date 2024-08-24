f1c65d2b9f4cc191bfe01d22fc72f70be065fba7
Lenilson Soares <lenilsonp40@gmail.com>
2024-08-24 10:25:08 -0300

    Tabuada

---
+++ ComponentesWebForms/ComponentesWebForms/Tabuada.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Tabuada.aspx.cs" Inherits="ComponentesWebForms.Tabuada" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>    
+    <form id="form1" runat="server">
+        <div>
+            <h1>Tabuada</h1>
+
+
+        </div>
+        <asp:DropDownList ID="dlNumeros" runat="server">
+        </asp:DropDownList>
+        <asp:Button ID="btExecutar" runat="server" OnClick="btExecutar_Click" Text="Exibir a Tabuada" />
+        <br />
+        <asp:ListBox ID="lbDados" runat="server" Height="251px"></asp:ListBox>
+    </form>
+</body>
+</html>
---
+++ ComponentesWebForms/ComponentesWebForms/Tabuada.aspx.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Esse código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace ComponentesWebForms
+{
+
+
+    public partial class Tabuada
+    {
+
+        /// <summary>
+        /// Controle form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Controle dlNumeros.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList dlNumeros;
+
+        /// <summary>
+        /// Controle btExecutar.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btExecutar;
+
+        /// <summary>
+        /// Controle lbDados.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox lbDados;
+    }
+}
