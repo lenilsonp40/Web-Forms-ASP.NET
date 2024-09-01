f8aec4e5cbe7af3821c48a6bfcbd3ba53ac8d846
Lenilson Soares <lenilsonp40@gmail.com>
2024-08-31 22:02:58 -0300

    wizard

---
+++ ProjetosWebForms/ComponentesWebForms/Wizard.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Wizard.aspx.cs" Inherits="ComponentesWebForms.Wizard" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Wizard ID="Wizard1" runat="server" ActiveStepIndex="0" BackColor="#FFFBD6" BorderColor="#FFDFAD" BorderWidth="1px" Font-Names="Verdana" Font-Size="0.8em" Height="386px" Width="436px">
+                <HeaderStyle BackColor="#FFCC66" BorderColor="#FFFBD6" BorderStyle="Solid" BorderWidth="2px" Font-Bold="True" Font-Size="0.9em" ForeColor="#333333" HorizontalAlign="Center" />
+                <NavigationButtonStyle BackColor="White" BorderColor="#CC9966" BorderStyle="Solid" BorderWidth="1px" Font-Names="Verdana" Font-Size="0.8em" ForeColor="#990000" />
+                <SideBarButtonStyle ForeColor="White" />
+                <SideBarStyle BackColor="#990000" Font-Size="0.9em" VerticalAlign="Top" />
+                <WizardSteps>
+                    <asp:WizardStep runat="server" title="Dados Báiscos">
+                        Nome<br />
+                        <asp:TextBox ID="tbNome" runat="server"></asp:TextBox>
+                        <br />
+                        CPF<br />
+                        <asp:TextBox ID="tbCPF" runat="server"></asp:TextBox>
+                        <br />
+                        RG<br />
+                        <asp:TextBox ID="tbRG" runat="server"></asp:TextBox>
+                    </asp:WizardStep>
+                    <asp:WizardStep runat="server" title="Endereço">
+                        CEP<br />
+                        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                        <br />
+                        Estado<br />
+                        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                        <br />
+                        Cidade<br />
+                        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+                        <br />
+                        Rua/Avenida<br />
+                        <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+                        <br />
+                        Bairro<br />
+                        <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+                    </asp:WizardStep>
+                </WizardSteps>
+            </asp:Wizard>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ProjetosWebForms/ComponentesWebForms/Wizard.aspx.designer.cs
@@ -0,0 +1,107 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Esse código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace ComponentesWebForms
+{
+
+
+    public partial class Wizard
+    {
+
+        /// <summary>
+        /// Controle form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Controle Wizard1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Wizard Wizard1;
+
+        /// <summary>
+        /// Controle tbNome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbNome;
+
+        /// <summary>
+        /// Controle tbCPF.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbCPF;
+
+        /// <summary>
+        /// Controle tbRG.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbRG;
+
+        /// <summary>
+        /// Controle TextBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+
+        /// <summary>
+        /// Controle TextBox2.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+
+        /// <summary>
+        /// Controle TextBox3.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox3;
+
+        /// <summary>
+        /// Controle TextBox4.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox4;
+
+        /// <summary>
+        /// Controle TextBox5.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox5;
+    }
+}
